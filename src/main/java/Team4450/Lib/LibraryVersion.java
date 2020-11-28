df2e087ce7a5176401e4990de0de0d7792c79765
Richard Corn <rac@swdevs.com>
2020-11-27 17:25:40 -0800

    Clean up and better document the release procedure.

--- src/main/java/Team4450/Lib/LibraryVersion.java
+++ src/main/java/Team4450/Lib/LibraryVersion.java
@@ -18,10 +18,11 @@ public class LibraryVersion
 	/*
 	 * When you start a new version of the library, you can change the version here
 	 * so that it is reflected by the robot programs that use the library. After a
-	 * compile, you go the robot program project's external references and do a
+	 * library compile, you go the robot program project's external references and do a
 	 * refresh to pull in the changes. You can push to github along the way to save
-	 * the work. Do not update any of the other locations where the version number
-	 * appears.
+	 * your work. You can also change src/main/resources/overview.html and gradle.properties
+	 * now or just before release. Build the library using Build RobotLib (or offline) not 
+	 * Build RobotLib (Release). You can push as needed as you work.
 	 * 
 	 * Note: The gradle process stores the compiled library in the local gradle
 	 * cache. The Robot program projects on this PC use a RobotLib.json file that
@@ -30,18 +31,18 @@ public class LibraryVersion
 	 * since the version, "local", never changes, you have to refresh the programs
 	 * external references to have the updated local version pulled into the project.
 	 * Robot projects on other PCs use a RobotLib.json with an actual version number
-	 * to pull that version from the online artifact library Jitpack.io.
+	 * to pull that version from the online artifact library Jitpack.io where our
+	 * releases are stored.
 	 * 
 	 * When you are ready to release, update the readme.md, src/main/resources/overview.html 
-	 * and gradle.properties files to the new version number. Do a final compile. Next, edit 
-	 * build.gradle file to switch version = "local" to version = LibraryVersion. Then push to 
-	 * github. After push switch the build.gradle version = back to "local". Then on the github 
-	 * repository you generate a new release for the new version. This will trigger a Travis 
-	 * compile on github which will generate the release artifacts and store the release artifacts 
-	 * on Jitpack.io. 
+	 * and gradle.properties files are set to the new version number. Do a final compile using
+	 * Build RobotLib (Release). Then push to github. Then on the github repository create a 
+	 * new release for the new version. This will trigger a Travis-ci compile (on github) which 
+	 * will generate the constituent release artifacts (files). It will also store the release 
+	 * artifacts on Jitpack.io repository. 
 	 * 
-	 * Robot program projects on other computers can then download or update RobotLib.json with. 
-	 * the new version number. On the next compile gradle will then pull down the new library 
+	 * Robot program projects on other computers can then download or update their RobotLib.json 
+	 * with  the new version number. On the next compile gradle will pull down the new library 
 	 * release from Jitpack.
 	 * 
 	 * Note: The Javadoc output is not pushed to github. Javadoc will be generated as an artifact 
