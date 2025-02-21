e254ad6ee72d042245faf98b7d1885c9b060e1bc
Richard Corn <rac@racc.com>
2025-02-20 18:55:04 -0800

    Forgot to save two files before final push.

--- src/main/java/Team4450/Lib/LibraryVersion.java
+++ src/main/java/Team4450/Lib/LibraryVersion.java
@@ -47,7 +47,7 @@ public class LibraryVersion
 	 * of the Jitpack compile when requested and will be automatically made available via JitPack. 
 	 */
 	
-	public static final String version = "4.11.0 (02.19.2025) WPILib=" + WPILibVersion.Version;	
+	public static final String version = "4.11.0 (02.20.2025) WPILib=" + WPILibVersion.Version;	
 	
 	// Private constructor means this class can't be instantiated.
 	private LibraryVersion()
